02eaf86e4e87f8a55e8ea18ca19fa66b9bf2e0f5
nisha_chen <nisha_chen@realsil.com.cn>
2022-09-14 16:51:54 +0800

    python: add a new article

---
+++ 5. 字符串/5.11 大小写转换-lower()&upper()&title().py
@@ -0,0 +1,19 @@
+# title() 函数，用于将字符串中每个单词的首字母转换成大写，注意：只将单词的首字母转换成大写，其它仍然是小写
+
+str = "math, Phy, english"
+str1 = str.title()
+print(str1) # 输出： Math, Phy, English
+
+
+# low() 函数，将所有字母转换成小写
+str = "Storm Sprint, OG, Topson, NOTAIL"
+str1 = str.lower()
+print(str1) # 输出：storm sprint, og, topson, notail
+
+# upper()函数，将所有字母转换成大写
+str = "ana, maybe, ame, topson"
+str1 = str.upper()
+print(str1) # 输出：ANA, MAYBE, AME, TOPSON
+
+
+
