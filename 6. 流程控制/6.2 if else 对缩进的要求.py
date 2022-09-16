d401024731fe3fd3af462ac827573b343f43ca90
nisha_chen <nisha_chen@realsil.com.cn>
2022-09-16 13:58:29 +0800

    python: add a new article

---
+++ 6. 流程控制/6.2 if else 对缩进的要求.py
@@ -0,0 +1,11 @@
+age = int(input("请输入年龄："))
+
+if age < 18 :
+    print("你是未成年人")
+else:
+    print("你是成年人")                     # 同一代码块的语句必须缩进，且缩进量必须相同
+    print("成年人要做好成年人该做的事情")
+
+
+print("OG TOPSON")
+
