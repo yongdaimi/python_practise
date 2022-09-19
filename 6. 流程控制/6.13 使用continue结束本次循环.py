2fa02de65cd114dc231954dd8305799acdcd2580
nisha_chen <nisha_chen@realsil.com.cn>
2022-09-19 11:11:12 +0800

    python: add a new article

---
+++ 6. 流程控制/6.13 使用continue结束本次循环.py
@@ -0,0 +1,20 @@
+
+for i in range(9):
+    if i == 2:
+        continue
+    print("i = ", i)
+
+print("程序执行完毕")
+
+'''
+输出：
+i =  0
+i =  1
+i =  3
+i =  4
+i =  5
+i =  6
+i =  7
+i =  8
+程序执行完毕
+'''
