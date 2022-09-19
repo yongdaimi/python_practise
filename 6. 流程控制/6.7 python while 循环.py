45e3d03806fcbd27b5e33b69727c73e91755f2b4
nisha_chen <nisha_chen@realsil.com.cn>
2022-09-19 10:19:50 +0800

    python: add a new article

---
+++ 6. 流程控制/6.7 python while 循环.py
@@ -0,0 +1,13 @@
+num = 1
+while num < 10:
+    print("%d" %num, end="")
+    num +=1
+
+print("\nprogram end")
+
+
+url = "http://www.baidu.com"
+i = 0
+while i < len(url):
+    print("url[i] = %c" %url[i])
+    i += 1
