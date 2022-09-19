968d627a8330e1c5589922370ded9339afb27de6
nisha_chen <nisha_chen@realsil.com.cn>
2022-09-19 10:54:09 +0800

    python: add a new article

---
+++ 6. 流程控制/6.10 循环嵌套.py
@@ -0,0 +1,11 @@
+# i = 0
+# while i < 10:
+#     for j in range(10):
+#         print("i=",i, "j=", j)
+#     i +=1
+
+
+i = 0
+if i < 10:
+    for j in range(5):
+        print("i =  ", i, " j = ", j)
