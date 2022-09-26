a82ec4d280cee63ac48e836f1d914dcbe4c09597
nisha_chen <nisha_chen@realsil.com.cn>
2022-09-26 11:06:23 +0800

    python: add a new article

---
+++ 7. 函数和lambda表达式/7.3 python 参数传递的内部机制.py
@@ -0,0 +1,18 @@
+def swap(a, b):
+    a, b = b, a
+    print("swap函数里, a = ", a, ", b = ", b)
+
+
+a = 10
+b = 20
+swap(a, b)
+print("当前a的值：", a, ", b的值：", b)
+
+list1 = [1, 2, 3, 4, 5]
+print(list1)
+
+# 清空 list1 的值
+list1 = None
+print(list1)
+
+
